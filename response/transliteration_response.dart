0a823d9f7806cb514ac5e4aad32231eb5855d681
jmd-2000 <157090973+jmd-2000@users.noreply.github.com>
2024-09-24 08:59:53 +0000

    Master

---
+++ response/transliteration_response.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+/// converts json to PODO class
+List<TransliterationResponse> transliterationResponseFromJson(String str) {
+  final jsonData = json.decode(str);
+  return new List<TransliterationResponse>.from(
+      jsonData.map((x) => TransliterationResponse.fromJson(x)));
+}
+
+/// converts back from PODO to json String
+String transliterationResponseToJson(List<TransliterationResponse> data) {
+  final dyn = new List<dynamic>.from(data.map((x) => x.toJson()));
+  return json.encode(dyn);
+}
+
+/// PODO class
+class TransliterationResponse {
+  String sourceString;
+  List<String> transliterationSuggestions;
+
+  TransliterationResponse({
+    this.sourceString = '',
+    this.transliterationSuggestions = const [],
+  });
+
+  factory TransliterationResponse.fromJson(Map<String, dynamic> json) =>
+      new TransliterationResponse(
+        sourceString: json["ew"],
+        transliterationSuggestions:
+            new List<String>.from(json["hws"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "ew": sourceString,
+        "hws": new List<String>.from(transliterationSuggestions.map((x) => x)),
+      };
+}
---
+++ src/main.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+import 'package:transliteration/response/transliteration_response.dart';
+import 'package:transliteration/src/utility.dart';
+import 'package:transliteration/transliteration.dart';
+
+/// The Fundamental class that provides static method to transliterate given string.
+class Transliteration {
+  /// This function transliterates english text to the given language code.
+  /// Exception will be thrown if user is not connected to internet or server is not responding.
+  /// Returns Input String and top 5 suggestions in a list for transliteration.
+  static Future<TransliterationResponse?> transliterate(
+    String inputString,
+    Languages languageCode,
+  ) async {
+    TransliterationResponse _transliterationResponse =
+        new TransliterationResponse();
+    String srcLanCode = Utility.getLanguageCode(Languages.ENGLISH);
+    String descLanCode = Utility.getLanguageCode(languageCode);
+    String lowerCaseSrcString = inputString.toLowerCase();
+    final transliterationURL =
+        '${Utility.transliterationBaseURL}?langpair=$srcLanCode|$descLanCode&text=$lowerCaseSrcString';
+    try {
+      Response _response = await http.get(Uri.parse(transliterationURL));
+      List<TransliterationResponse> _transliterationResponseList =
+          transliterationResponseFromJson(_response.body.toString());
+      _transliterationResponse.transliterationSuggestions =
+          _transliterationResponseList.first.transliterationSuggestions;
+      _transliterationResponse.sourceString =
+          _transliterationResponseList.first.sourceString;
+      return _transliterationResponse;
+    } catch (e) {
+      print('Exception : $e');
+    }
+    return null;
+  }
+}
